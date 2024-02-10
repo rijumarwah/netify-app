21c0ad9096196d9c22c1f614d2bd823c19ab4ae4
riju <marwah.riju@gmail.com>
2024-02-10 23:03:19 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,17 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>retro bin</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Riju's bin" />
+        <Header title="" />
         <p className="description">
           Riju's <code>code bin</code>
         </p>
+        <i><a href="//24timezones.com/world_directory/current_salt_lake_city_time.php" style="text-decoration: none" class="clock24" id="tz24-1562550567-c1220-eyJob3VydHlwZSI6MTIsInNob3dkYXRlIjoiMCIsInNob3dzZWNvbmRzIjoiMSIsInNob3d0aW1lem9uZSI6IjEiLCJ0eXBlIjoiZCIsImxhbmciOiJlbiJ9" title="Salt Lake City Time" target="_blank" rel="nofollow">TIME IN SALT LAKE CITY</a>
+        </i><script type="text/javascript" src="//w.24timezones.com/l.js" async></script>
       </main>
 
       <Footer />
